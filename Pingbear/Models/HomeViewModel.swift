e224e8e58d9442a4c16028e5905d8d89d364c474
chisom123 <chisagu5@gmail.com>
2023-09-21 17:00:47 +0100

    Optimized home view model

--- Pingbear/Models/HomeViewModel.swift
+++ Pingbear/Models/HomeViewModel.swift
@@ -6,6 +6,7 @@ import PhoneNumberKit
 import Flurry_iOS_SDK
 
 class HomeViewModel: ObservableObject {
+    
     @Published var appUsers: [AppUser] = []
     @Published var isChatViewPresented: Bool = false
     @Published var isSearchViewPresented: Bool = false
@@ -13,7 +14,7 @@ class HomeViewModel: ObservableObject {
     @Published var currentIndex: Int = 0
     @Published var activeSheet: ActiveSheet? = nil
     @Published var hasLoadedBefore: Bool = false
-    @Published var selectedUserIndex: Int? = nil {
+    @Published var selectedUserIndex: Int? {
         didSet {
             if let index = selectedUserIndex {
                 currentIndex = index
@@ -22,22 +23,23 @@ class HomeViewModel: ObservableObject {
         }
     }
     
+    private let db = Firestore.firestore()
+    private let phoneNumberKit = PhoneNumberKit()
+    private let usersPath = "users"
+    private let friendsPath = "friends"
+    
     enum ActiveSheet: Identifiable {
         case chatView, searchView, bearsView, addFriendsView
-
-        var id: Int {
-            hashValue
-        }
+        
+        var id: Int { hashValue }
     }
-
+    
     private func fetchUserDetails(friendIDs: [String]) {
-        let db = Firestore.firestore()
-
-        // Use DispatchGroup to manage asynchronous operations
         let group = DispatchGroup()
+        
         for friendID in friendIDs {
             group.enter()
-            let docRef = db.collection("users").document(friendID)
+            let docRef = db.collection(usersPath).document(friendID)
             
             docRef.getDocument { (document, error) in
                 defer { group.leave() }
@@ -45,34 +47,33 @@ class HomeViewModel: ObservableObject {
                     print("Error getting friend's details: \(error)")
                     return
                 }
-                guard let doc = document, doc.exists, let data = doc.data() else { return }
-
-                let friendPhoneNumber = data["phoneNumber"] as? String
-                let friendName = data["name"] as? String
-                let friendIcon = data["icon"] as? String
-                    
-                // Removed the lastMessageTimestamp part
-                let user = AppUser(id: friendID, name: friendName ?? "", phoneNumber: friendPhoneNumber ?? "", icon: friendIcon)
+                
+                guard let data = document?.data() else { return }
+                let user = AppUser(id: friendID,
+                                   name: data["name"] as? String ?? "",
+                                   phoneNumber: data["phoneNumber"] as? String ?? "",
+                                   icon: data["icon"] as? String)
                 
                 DispatchQueue.main.async {
-                    if let index = self.appUsers.firstIndex(where: { $0.id == friendID }) {
-                        self.appUsers[index] = user
-                    } else {
-                        self.appUsers.append(user)
-                    }
+                    self.updateOrAddUser(user)
                 }
             }
         }
-
-        // Once all users have been fetched, shuffle the appUsers array
+        
         group.notify(queue: .main) {
             self.appUsers.shuffle()
         }
     }
+    
+    private func updateOrAddUser(_ user: AppUser) {
+        if let index = appUsers.firstIndex(where: { $0.id == user.id }) {
+            appUsers[index] = user
+        } else {
+            appUsers.append(user)
+        }
+    }
 
-    func formatPhoneNumber(_ phoneNumber: String) -> String {
-        let phoneNumberKit = PhoneNumberKit()
-
+    private func formatPhoneNumber(_ phoneNumber: String) -> String {
         do {
             let parsedPhoneNumber = try phoneNumberKit.parse(phoneNumber)
             return phoneNumberKit.format(parsedPhoneNumber, toType: .e164)
@@ -81,24 +82,21 @@ class HomeViewModel: ObservableObject {
         }
     }
     
-    func fetchContacts() {
+    private func fetchContacts() {
         guard let currentUserID = Auth.auth().currentUser?.uid else {
             print("Failed to get current user ID")
             return
         }
-        
-        let db = Firestore.firestore()
-        
-        // First, get the current user's friends
-        db.collection("users").document(currentUserID).collection("friends").getDocuments { (snapshot, error) in
+
+        db.collection(usersPath).document(currentUserID).collection(friendsPath).getDocuments { (snapshot, error) in
             if let error = error {
                 print("Error getting friends: \(error)")
                 return
             }
+
             let friendIDs = snapshot?.documents.compactMap { $0["uid"] as? String } ?? []
             self.fetchUserDetails(friendIDs: friendIDs)
-            
-            // Add mutual contacts to friends collection if not added yet
+
             DispatchQueue.global().async {
                 let store = CNContactStore()
                 store.requestAccess(for: .contacts) { (granted, error) in
@@ -106,40 +104,49 @@ class HomeViewModel: ObservableObject {
                         print("Failed to request access: \(error)")
                         return
                     }
+
                     if granted {
-                        let keys = [CNContactPhoneNumbersKey as CNKeyDescriptor]
-                        let request = CNContactFetchRequest(keysToFetch: keys)
-                        do {
-                            try store.enumerateContacts(with: request, usingBlock: { (contact, stopPointer) in
-                                for phoneNumber in contact.phoneNumbers {
-                                    let number = phoneNumber.value.stringValue
-                                    let formattedNumber = self.formatPhoneNumber(number)
-                                    
-                                    db.collection("users").whereField("phoneNumber", isEqualTo: formattedNumber).getDocuments { (snapshot, error) in
-                                        if let error = error {
-                                            print("Error getting documents: \(error)")
-                                            return
-                                        }
-                                        for document in snapshot!.documents {
-                                            let mutualContactID = document.documentID
-                                            if !friendIDs.contains(mutualContactID) {
-                                                // If this contact isn't already a friend, add to friends collection
-                                                db.collection("users").document(currentUserID).collection("friends").document(mutualContactID).setData(["uid": mutualContactID])
-                                                db.collection("users").document(mutualContactID).collection("friends").document(currentUserID).setData(["uid": currentUserID])
-                                                
-                                                Flurry.log(eventName: "Friends-Added")
-                                            }
-                                        }
-                                    }
-                                }
-                            })
-                        } catch let error {
-                            print("Failed to enumerate contacts: \(error)")
-                        }
+                        self.processContacts(store: store, currentUserId: currentUserID, friendIDs: friendIDs)
                     }
                 }
             }
         }
+    }
+
+    private func processContacts(store: CNContactStore, currentUserId: String, friendIDs: [String]) {
+        let keys = [CNContactPhoneNumbersKey as CNKeyDescriptor]
+        let request = CNContactFetchRequest(keysToFetch: keys)
+        
+        do {
+            try store.enumerateContacts(with: request, usingBlock: { (contact, _) in
+                for phoneNumber in contact.phoneNumbers {
+                    let number = phoneNumber.value.stringValue
+                    let formattedNumber = self.formatPhoneNumber(number)
+                    
+                    db.collection(self.usersPath).whereField("phoneNumber", isEqualTo: formattedNumber).getDocuments { (snapshot, error) in
+                        if let error = error {
+                            print("Error getting documents: \(error)")
+                            return
+                        }
+                        
+                        for document in snapshot!.documents {
+                            let mutualContactID = document.documentID
+                            if !friendIDs.contains(mutualContactID) {
+                                self.addFriend(currentUserId: currentUserId, friendId: mutualContactID)
+                            }
+                        }
+                    }
+                }
+            })
+        } catch let error {
+            print("Failed to enumerate contacts: \(error)")
+        }
+    }
+
+    private func addFriend(currentUserId: String, friendId: String) {
+        db.collection(usersPath).document(currentUserId).collection(friendsPath).document(friendId).setData(["uid": friendId])
+        db.collection(usersPath).document(friendId).collection(friendsPath).document(currentUserId).setData(["uid": currentUserId])
+        Flurry.log(eventName: "Friends-Added")
     }
     
     func fetchFriends() {
@@ -148,28 +155,22 @@ class HomeViewModel: ObservableObject {
             return
         }
         
-        let db = Firestore.firestore()
-
         if !hasLoadedBefore {
-            // Use a realtime listener to fetch the current user's friends from Firestore
-            db.collection("users").document(currentUserID).collection("friends").addSnapshotListener { (snapshot, error) in
+            db.collection(usersPath).document(currentUserID).collection(friendsPath).addSnapshotListener { (snapshot, error) in
                 if let error = error {
                     print("Error getting friends: \(error)")
                     return
                 }
+
                 let friendIDs = snapshot?.documents.compactMap { $0["uid"] as? String } ?? []
                 self.fetchUserDetails(friendIDs: friendIDs)
-                
                 self.fetchContacts()
             }
             hasLoadedBefore = true
         } else {
-            // If already loaded before, directly fetch contacts without setting up a new listener
             self.fetchContacts()
         }
     }
-
-
 
     func selectUser(_ user: AppUser) {
         selectedUser = user
