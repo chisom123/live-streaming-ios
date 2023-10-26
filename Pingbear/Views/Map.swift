12d373745118addf6e671b4cf295ce8ff73561cf
chisom123 <chisagu5@gmail.com>
2023-10-26 20:56:33 +0100

    Can now only see competitions in a 5 mile radius

--- Pingbear/Views/Map.swift
+++ Pingbear/Views/Map.swift
@@ -6,61 +6,89 @@ import FirebaseFirestore
 import Flurry_iOS_SDK
 import CoreLocation
 
-class CustomPointAnnotation: NSObject, MKAnnotation, Identifiable {
-    let id = UUID() // Conform to Identifiable
-    @objc dynamic var coordinate: CLLocationCoordinate2D // Required by MKAnnotation
-    var title: String? // Optional - you can add additional data if needed
-
-    init(coordinate: CLLocationCoordinate2D) {
-        self.coordinate = coordinate
-    }
-}
-
 struct MapView: View {
+    // Reduced the span for a more zoomed-in view
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02) // smaller delta means more zoom
     )
-    
-    // Corrected to use CustomPointAnnotation
+
     @State private var competitionLocations = [CustomPointAnnotation]()
     
     private var locationManager = CLLocationManager()
     
     var body: some View {
         Map(coordinateRegion: $region,
-            showsUserLocation: true, // This line is important
+            showsUserLocation: true,
             annotationItems: competitionLocations) { location in
                 MapAnnotation(coordinate: location.coordinate) {
-                    Image(systemName: "mappin.circle.fill")
+                    Image(systemName: "star.fill")
                         .foregroundColor(.red)
                         .imageScale(.large)
                 }
             }
-        .edgesIgnoringSafeArea(.top) // for better display
+        .edgesIgnoringSafeArea(.top)
         .onAppear {
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.requestWhenInUseAuthorization()
-            locationManager.startUpdatingLocation()
+            setupLocationManager()
+            fetchCompetitionLocations()
+        }
+    }
 
-            if let location = locationManager.location {
-                region.center = location.coordinate
-            }
+    func distance(from location1: CLLocationCoordinate2D, to location2: CLLocationCoordinate2D) -> CLLocationDistance {
+        let location1 = CLLocation(latitude: location1.latitude, longitude: location1.longitude)
+        let location2 = CLLocation(latitude: location2.latitude, longitude: location2.longitude)
 
-            let db = Firestore.firestore()
-            db.collection("competitions").getDocuments { (querySnapshot, err) in
-                if let err = err {
-                    print("Error getting documents: \(err)")
-                } else {
-                    competitionLocations = querySnapshot?.documents.compactMap { document in
-                        let data = document.data()
-                        if let lat = data["latitude"] as? Double, let lon = data["longitude"] as? Double {
-                            return CustomPointAnnotation(coordinate: .init(latitude: lat, longitude: lon))
+        return location1.distance(from: location2) // returns the distance in meters
+    }
+
+
+    private func setupLocationManager() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+        // Set the initial region to the user's current location
+        if let userLocation = locationManager.location?.coordinate {
+            region = MKCoordinateRegion(
+                center: userLocation,
+                span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+            )
+        }
+    }
+
+    private func fetchCompetitionLocations() {
+        let db = Firestore.firestore()
+        db.collection("competitions").getDocuments { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                let userLocation = locationManager.location?.coordinate
+                competitionLocations = querySnapshot?.documents.compactMap { document in
+                    let data = document.data()
+                    if  let lat = data["latitude"] as? Double,
+                        let lon = data["longitude"] as? Double {
+                        let competitionLocation = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+
+                        // Check if competition location is within a 5-mile (about 8046.72 meters) radius
+                        if let userLocation = userLocation,
+                           distance(from: userLocation, to: competitionLocation) <= 8046.72 {
+                            return CustomPointAnnotation(coordinate: competitionLocation)
                         }
-                        return nil
-                    } ?? []
-                }
+                    }
+                    return nil
+                } ?? []
             }
         }
+    }
+
+}
+
+class CustomPointAnnotation: NSObject, MKAnnotation, Identifiable {
+    let id = UUID()
+    @objc dynamic var coordinate: CLLocationCoordinate2D
+    var title: String?
+
+    init(coordinate: CLLocationCoordinate2D) {
+        self.coordinate = coordinate
     }
 }
