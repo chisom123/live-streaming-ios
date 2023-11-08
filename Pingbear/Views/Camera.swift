66ee12d4ea9f831adfb20c5be37c2023a55fb235
chisom123 <chisagu5@gmail.com>
2023-11-08 22:09:17 +0000

    Started working on the new camera set up

--- Pingbear/Views/Camera.swift
+++ Pingbear/Views/Camera.swift
@@ -1,16 +1,100 @@
 import SwiftUI
-import UIKit
 import SwiftttCamera
 
-struct CameraView: View {
+struct CameraViewControllerRepresentable: UIViewControllerRepresentable {
+    @Binding var shouldToggleCamera: Bool
+    
+    func makeUIViewController(context: Context) -> CameraViewController {
+        let cameraViewController = CameraViewController()
+        return cameraViewController
+    }
 
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        if shouldToggleCamera {
+            uiViewController.toggleCamera()
+            DispatchQueue.main.async {
+                self.shouldToggleCamera = false
+            }
+        }
+    }
+
+    typealias UIViewControllerType = CameraViewController
+}
+
+class CameraViewController: UIViewController, CameraDelegate {
+    private lazy var camera: SwiftttCamera = {
+        let result = SwiftttCamera()
+        result.delegate = self
+        result.view.translatesAutoresizingMaskIntoConstraints = false
+        return result
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        swiftttAddChild(camera)
+        camera.view.frame = view.frame
+    }
+
+    func cameraController(_ cameraController: CameraProtocol, didFinishCapturingImage capturedImage: CapturedImage) {
+        // Handle the captured image
+    }
+
+    func toggleCamera() {
+        let newCameraDevice: CameraDevice = camera.cameraDevice.toggling()
+        guard SwiftttCamera.isCameraDeviceAvailable(newCameraDevice) else { return }
+        camera.cameraDevice = newCameraDevice
+    }
+}
+
+struct CameraView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var shouldToggleCamera = false
+    
     var body: some View {
-        var camera: SwiftttCamera = {
-            let result = SwiftttCamera()
-            result.view.translatesAutoresizingMaskIntoConstraints = false
-            return result
-        }()
-        
-        
+        ZStack {
+            CameraViewControllerRepresentable(shouldToggleCamera: $shouldToggleCamera)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "arrow.left")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.white.opacity(0.5))
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                    }
+                    Spacer()
+                    Button(action: {
+                        self.shouldToggleCamera.toggle()
+                    }) {
+                        Image(systemName: "camera.rotate")
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                    }
+                }
+                .padding([.top, .leading, .trailing])
+                Spacer() // This will push the top HStack to the top and the bottom button to the bottom
+                
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "camera.circle")
+                        .font(.system(size: 72))
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .shadow(radius: 10)
+                }
+                .padding(.bottom)
+            }
+        }
     }
 }
--- Pingbear/Views/NewCompetition.swift
+++ Pingbear/Views/NewCompetition.swift
@@ -102,17 +102,17 @@ struct NewCompetition: View {
             "timestamp": Timestamp() // Current time
         ]
         
-        
+        self.isCameraPresented = true
         // Add a new document with the competition data
-        db.collection("competitions").addDocument(data: competitionData) { err in
-            if let err = err {
-                print("Error adding document: \(err)")
-            } else {
-                print("Document added")
-                // You can dismiss the current view or do something else
-                self.isCameraPresented = true
-            }
-        }
+//        db.collection("competitions").addDocument(data: competitionData) { err in
+//            if let err = err {
+//                print("Error adding document: \(err)")
+//            } else {
+//                print("Document added")
+//                // You can dismiss the current view or do something else
+//                self.isCameraPresented = true
+//            }
+//        }
     }
 
 }
