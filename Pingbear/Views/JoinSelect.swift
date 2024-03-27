fb1e7f79cfd57cc9ec3dabf7e30cd142de873c4e
chisom123 <chisagu5@gmail.com>
2024-03-27 21:20:46 +0000

    Made it so you can select who can join and vote these competitions

---
+++ Pingbear/Views/JoinSelect.swift
@@ -0,0 +1,8 @@
+//
+//  JoinSelect.swift
+//  Pingbear
+//
+//  Created by Ezi Agu on 08/01/1403 AP.
+//
+
+import Foundation
---
+++ Pingbear/Views/VoteSelect.swift
@@ -0,0 +1,8 @@
+//
+//  VoteSelect.swift
+//  Pingbear
+//
+//  Created by Ezi Agu on 08/01/1403 AP.
+//
+
+import Foundation
