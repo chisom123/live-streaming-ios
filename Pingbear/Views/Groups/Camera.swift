006387666b1adfe2d10fbf22b7574c57fd543dff
chisom123 <chisagu5@gmail.com>
2024-04-26 17:08:32 +0100

    Removed some unecessary code from the camera view

--- Pingbear/Views/Groups/Camera.swift
+++ Pingbear/Views/Groups/Camera.swift
@@ -246,8 +246,6 @@ struct CameraView: View {
             .fullScreenCover(isPresented: $shouldNavigateToLocationCheck) {
                 if let entryDocId = newentryDocId {
                     LocationCheckView(competition: competition, competitionId: competitionId, entryDocId: entryDocId)
-                } else {
-                    
                 }
             }
             .sheet(isPresented: $isShowingImagePicker) {
@@ -323,33 +321,6 @@ struct CameraView: View {
                         print("Error saving entry: \(error)")
                     } else {
                         print("Entry saved successfully")
-            
-                        // Add the user to the participants collection
-                        let participantRef = db.collection("competitions").document(self.competitionId).collection("participants").document(userId)
-                        
-                        participantRef.getDocument { documentSnapshot, error in
-                            if let documentSnapshot = documentSnapshot, documentSnapshot.exists {
-                                // Document exists, update it without overwriting existing data
-                                let updates = ["userId": userId] // Assuming you want to ensure the userId is correct
-                                participantRef.updateData(updates) { error in
-                                    if let error = error {
-                                        print("Error updating participant: \(error)")
-                                    } else {
-                                        print("Participant updated successfully.")
-                                    }
-                                }
-                            } else {
-                                // Document does not exist, create a new one
-                                let newData = ["userId": userId, "voted_entries": []] // Initializes voted_entries as empty
-                                participantRef.setData(newData) { error in
-                                    if let error = error {
-                                        print("Error creating new participant: \(error)")
-                                    } else {
-                                        print("New participant created successfully.")
-                                    }
-                                }
-                            }
-                        }
                         
                         self.newentryDocId = newEntryRef?.documentID
                         
