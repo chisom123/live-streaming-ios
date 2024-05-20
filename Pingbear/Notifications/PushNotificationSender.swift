97858052df65082b931120b0742f161a154d2ad0
chisom123 <chisagu5@gmail.com>
2024-05-20 14:36:07 +0100

    Updated Notification Sender to work dynamically using fierbase function

--- Pingbear/Notifications/PushNotificationSender.swift
+++ Pingbear/Notifications/PushNotificationSender.swift
@@ -3,9 +3,24 @@ import FirebaseMessaging
 import Foundation
 import PostHog
 
+struct TokenResponse: Codable {
+    var accessToken: String
+}
+
 class PushNotificationSender: ObservableObject {
     func sendPushNotification(to token: String, title: String, body: String) {
-        let projectID = "pingbear-96b4c" // Replace with your actual project ID
+        fetchAccessToken { result in
+            switch result {
+            case .success(let accessToken):
+                self.postNotification(to: token, title: title, body: body, accessToken: accessToken)
+            case .failure(let error):
+                print("Failed to fetch access token: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    private func postNotification(to token: String, title: String, body: String, accessToken: String) {
+        let projectID = "pingbear-96b4c"  // Replace with your actual project ID
         let urlString = "https://fcm.googleapis.com/v1/projects/\(projectID)/messages:send"
         guard let url = URL(string: urlString) else { return }
         
@@ -22,11 +37,7 @@ class PushNotificationSender: ObservableObject {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.httpBody = try? JSONSerialization.data(withJSONObject: message, options: [])
-        request.setValue("application/json; UTF-8", forHTTPHeaderField: "Content-Type")
-        
-        // Fetching an access token using Google's Authentication libraries
-        // This part needs to be implemented according to your server setup
-        let accessToken = fetchAccessToken()  // Implement this function based on your authentication setup
+        request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
         request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
@@ -39,9 +50,35 @@ class PushNotificationSender: ObservableObject {
         task.resume()
     }
     
-    private func fetchAccessToken() -> String {
-        // Implement token generation or retrieval logic here
-        // For example, using Google's Application Default Credentials (ADC)
-        return "ya29.c.c0AY_VpZjfIKU_dH_s90UssoBC9rYFpGJPNasUuRI7tSfbJDXmIGUNaJPY0JihJIuHA2Jzr1GKCdIc7fl8pkh0f0Rl0wjetOQz_ozoDABeNOGVeN-KPwAFzidsZlc-H8JniMca2vdef7Ob9A0oIgdBIsJ5FFj3l2yENJhurX0z49lABsfybIYW0SZXKDN1Z5be5OhE1sJ6NTFah_VvbTgLsfJGcgoFK2P7x3DiWr_0WEuBBraENJcjaVbMYTaIGGGdGP7O90feXEbSmpsV0CzYPffp_blQE-yiyeJaJIc5d21SaorpN3UTias4ytAdheGppnJ-fXBpB1B8uNXBCzq4upxdGzd65-lTEz4qLXMzNA7eiqzUE4NbnSYE384DjXBt3s94dSYjoXRwv_eF9o1uk_YieV1q28h7R1zYJg_x84I3l_zVo0IYBgYgM64SkphgqukvW1V7q399ssQWyowza4oxZc_h1V07XzBWl5OBbhmr0MtySqjWsQjo3rjdrBjj9uM4_uomlM8ruRfu4kmrj9ReZdVqbmJdBnQXO_BZtc5gcl8r7jnUmXphSd7u72QweeiSdBI0Yv2kiSRbOUJV_-3zf5RRcph-ZvsiQtlQcURF10z_JjR04YxnqFoscVMXndp-egYO8l9X5RscYFOe8-5ni4cYes66I5tebjSImM3UqWY3lYIWsla9mIhy-m8VaXi43iOgRgk86F0wy9v_J2y6p9s2Yv9hRWbrjX6w_pq2oFho3FWkM-5OcO-eFhhyloBFyJqph5kbWwF4_7-xU19vramg7wI33Sp9fclOynSofsU1w3JSmqupZI0v2rdX-MxUu-tvwtfxnY50agF4uue8-j_-Ja-Xq1qoVh--e_4OyuXnzy0-coi8zltpwUSgo-jZ9fnj99vkp6qh2xg4Ygkfo2dQx38i5Q3vWqQdxMjm2Zlmry0noIf3-gx_-5Fd7r6f-YrR0RMzZtpZppbWezSIb_-klwdYWj2UdX2wgQdVU7FR1vk-VBm"  // Replace with actual token fetching logic
+    func fetchAccessToken(completion: @escaping (Result<String, Error>) -> Void) {
+        guard let url = URL(string: "https://getaccesstoken-4lnpddia6q-uc.a.run.app") else {
+            print("Invalid URL")
+            completion(.failure(NSError(domain: "URLInvalidError", code: 1, userInfo: nil)))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(TokenResponse.self, from: data) {
+                    DispatchQueue.main.async {
+                        completion(.success(decodedResponse.accessToken))
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(.failure(NSError(domain: "JSONDecodingError", code: 2, userInfo: nil)))
+                    }
+                }
+            }
+        }.resume()
     }
 }
