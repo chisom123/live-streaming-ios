b728b6d13e46c1c7e519422afbfc5f65b3ce7ff0
chisom123 <chisagu5@gmail.com>
2025-01-15 23:15:33 +0000

    Removed boost status view

--- Pingbear/Views/User Management/Settings.swift
+++ Pingbear/Views/User Management/Settings.swift
@@ -7,7 +7,6 @@ struct SettingsView: View {
     
     @Environment(\.presentationMode) var presentationMode
     @State private var showChangeNameView = false  // State to control the full screen cover for ChangeNameView
-    @State private var showBoostView = false  // State to control the full screen cover for ChangeNameView
     @State private var showMyFriendsView = false  // State to control the full screen cover for ChangeNameView
     @State private var showAddFriendsView = false  // State to control the full screen cover for ChangeNameView
     @State private var showSignOutAlert = false
@@ -99,6 +98,24 @@ struct SettingsView: View {
                                 MyFriendsView(viewModel: MyFriendsModel())
                             }
                             
+                            Button(action: {
+                                self.showChangeNameView = true // Toggle the state to show the ChangeNameView
+                            }) {
+                                HStack {
+                                    Text("My Username")
+                                        .font(.system(size: 16, weight: .bold, design: .default))
+                                        .foregroundColor(Color(hex: "#1199FF"))
+                                    Spacer()
+                                }
+                                .padding([.top, .bottom], 20)
+                                .padding([.leading, .trailing], 20)
+                            }
+                            .background(Color(hex: "#F5F5F5"))
+                            .cornerRadius(5)
+                            .fullScreenCover(isPresented: $showChangeNameView) {  // Use the full screen cover modifier
+                                ChangeNameView()
+                            }
+                            
                             if AdminAuthManager.shared.isAdmin {
                                 Button(action: {
                                     self.showAdminDashboard = true
@@ -121,44 +138,6 @@ struct SettingsView: View {
                         }
                         
                         VStack(spacing: 25) {
-                            
-                            // Change Name Button
-                            Button(action: {
-                                self.showChangeNameView = true // Toggle the state to show the ChangeNameView
-                            }) {
-                                HStack {
-                                    Text("My Username")
-                                        .font(.system(size: 16, weight: .bold, design: .default))
-                                        .foregroundColor(Color(hex: "#1199FF"))
-                                    Spacer()
-                                }
-                                .padding([.top, .bottom], 20)
-                                .padding([.leading, .trailing], 20)
-                            }
-                            .background(Color(hex: "#F5F5F5"))
-                            .cornerRadius(5)
-                            .fullScreenCover(isPresented: $showChangeNameView) {  // Use the full screen cover modifier
-                                ChangeNameView()
-                            }
-                            
-                            Button(action: {
-                                self.showBoostView = true // Toggle the state to show the ChangeNameView
-                            }) {
-                                HStack {
-                                    Text("Boost Status")
-                                        .font(.system(size: 16, weight: .bold, design: .default))
-                                        .foregroundColor(Color(hex: "#1199FF"))
-                                    Spacer()
-                                }
-                                .padding([.top, .bottom], 20)
-                                .padding([.leading, .trailing], 20)
-                            }
-                            .background(Color(hex: "#F5F5F5"))
-                            .cornerRadius(5)
-                            .fullScreenCover(isPresented: $showBoostView) {  // Use the full screen cover modifier
-                                BoostView(viewModel: BoostViewModel())
-                            }
-                            
                             Button(action: {
                                 if let url = URL(string: "mailto:pingbearapp@gmail.com") {
                                     UIApplication.shared.open(url)
@@ -167,7 +146,7 @@ struct SettingsView: View {
                                 HStack {
                                     Text("Contact Us")
                                         .font(.system(size: 16, weight: .bold, design: .default))
-                                        .foregroundColor(Color(hex: "#1199FF"))
+                                        .foregroundColor(Color(hex: "#ababab"))
                                     Spacer()
                                 }
                                 .padding([.top, .bottom], 20)
@@ -175,9 +154,6 @@ struct SettingsView: View {
                             }
                             .background(Color(hex: "#F5F5F5"))
                             .cornerRadius(5)
-                        }
-                        
-                        VStack(spacing: 25) {
                             
                             // Log Out Button
                             Button(action: {
