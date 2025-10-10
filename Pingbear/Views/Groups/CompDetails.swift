5df089c9efaef78e7c9beb4ac39058a05094b812
chisom123 <chisagu5@gmail.com>
2025-10-10 19:12:27 +0100

    Add haptic on start rating button

--- Pingbear/Views/Groups/CompDetails.swift
+++ Pingbear/Views/Groups/CompDetails.swift
@@ -137,6 +137,8 @@ struct CompDetails: View {
                     .disabled(!entryViewModel.hasEntriesToVoteOn)
                     .simultaneousGesture(TapGesture().onEnded {
                         entryViewModel.removeListeners()
+                        let generator = UINotificationFeedbackGenerator()
+                        generator.notificationOccurred(.success)
                         Analytics.shared.trackEntry(
                             action: "rate",
                             competitionId: competition.id
